746d5cba997075ce231b677379c9820ba5399063
prabhjot730227 <prabhjot7brar@gmail.com>
2019-01-24 15:23:21 -0500

    now i commit

--- c0730227/Program.cs
+++ c0730227/Program.cs
@@ -10,6 +10,23 @@ namespace c0730227
     {
         static void Main(string[] args)
         {
+           var dog_and_cat = new Car();
+            var bowling = new Car();
+            Console.WriteLine("how many cars: {0}", Vehicle.howManyCars);
+        }
+    }
+    class Vehicle
+    {
+        public static int howManyCars=0;
+        public String Color = "Blue";
+    }
+    class Car : Vehicle
+    {
+        public String Color = "Red";
+        public Car()
+        {
+            Vehicle.howManyCars++;
+            Console.WriteLine("my color is " + Color);
         }
     }
 }
