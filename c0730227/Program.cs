660b80636a0d77feb284dc5cfce2c0c4b59af37a
prabhjot730227 <prabhjot7brar@gmail.com>
2019-01-24 16:09:38 -0500

    Now i commit

--- c0730227/Program.cs
+++ c0730227/Program.cs
@@ -6,13 +6,41 @@ using System.Threading.Tasks;
 
 namespace c0730227
 {
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public void Promote() {
+            var rating = CalculateRating();
+                if (rating == 0)
+            {
+                Console.WriteLine("Promoted to Level 1");
+
+            }
+            else
+            {
+                Console.WriteLine("Promoted to Level 2");
+            }
+        }
+        public int CalculateRating()
+        {
+            return 0;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-           var dog_and_cat = new Car();
-            var bowling = new Car();
-            Console.WriteLine("how many cars: {0}", Vehicle.howManyCars);
+            var customer = new Customer();
+            
+            //var dog_and_cat = new Car();
+            // var bowling = new Car();
+            // Console.WriteLine("how many cars: {0}", Vehicle.howManyCars);
+            //Book myBook = new Book();
+            //myBook.SetTitle("A Separate Peace");
+            //Console.WriteLine("my color is " + Color);
+
         }
     }
     class Vehicle
@@ -24,9 +52,22 @@ namespace c0730227
     {
         public String Color = "Red";
         public Car()
+      {
+           Vehicle.howManyCars++;
+           Console.WriteLine("my color is " + Color);
+      }
+    class BookCollection
         {
-            Vehicle.howManyCars++;
-            Console.WriteLine("my color is " + Color);
+
+        }
+        class Book : BookCollection
+        {
+            private String Title;
+            public void SetTitle(String aTitle)
+            {
+                this.Title = aTitle;
+            }
+            public String GetTitle() { return this.Title; }
         }
     }
-}
+
